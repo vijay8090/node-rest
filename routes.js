5222dabda770d31c52c87e6c15d2ca886218de42
Vijayakumar <vijayakumar.anbu1@gmail.com>
2021-09-05 21:23:26 +0530

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const employees = require("./data/employees.js");
-
+const productCategories = require("./data/productCategories.js");
+const products = require("./data/products.js");
 router.get("/", (req, res) => {
   res.send("App is running");
 });
 
 router.get("/employee", (req, res) => {
   res.status(200).json(employees);
+});
+router.get("/product", (req, res) => {
+  res.status(200).json(products);
+});
+
+router.get("/product-category", (req, res) => {
+  res.status(200).json(productCategories);
 });
 
 
