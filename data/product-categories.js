46c0690b18bf177154d19e489dcc9962238345ea
Vijayakumar Anbu <vanbu@woundtech.net>
2021-09-18 11:00:48 +0530

    category

--- data/product-categories.js
+++ data/product-categories.js
@@ -1,2 +1,2 @@
-const productCategories = ["accessories", "apparel", "bags", "drinkware", "office", "brand"];
+const productCategories = ["accessories", "apparel", "bags", "drinkware", "office", "shop by brand"];
 module.exports = productCategories;
