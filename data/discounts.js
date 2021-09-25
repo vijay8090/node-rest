cc99361b17883fbb3467725d1489e4dcc5c02116
Vijayakumar Anbu <vanbu@woundtech.net>
2021-09-25 09:56:48 +0530

    discounts added

---
+++ data/discounts.js
@@ -0,0 +1,34 @@
+const discountsByProduct = [
+  {
+    id: "GGOEAFKA087499",
+    discountPercentage: 10,
+  },
+  {
+    id: "GGOEAFKA087499",
+    discountPercentage: 10,
+  },
+];
+
+const discountsByCategory = [
+  {
+    category: "accessories",
+    discountPercentage: 5,
+  },
+  {
+    id: "apparel",
+    discountPercentage: 10,
+  },
+  {
+    category: "bags",
+    discountPercentage: 15,
+  },
+  {
+    id: "drinkware",
+    discountPercentage: 10,
+  },
+];
+
+module.exports = {
+  discountsByProduct,
+  discountsByCategory,
+};
--- routes.js
+++ routes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const employees = require("./data/employees.js");
 const productCategories = require("./data/product-categories.js");
 const products = require("./data/products.js");
+const discounts = require("./data/discounts.js");
+
 router.get("/", (req, res) => {
   res.send("App is running");
 });
@@ -13,6 +15,10 @@ router.get("/employee", (req, res) => {
 
 router.get("/product", (req, res) => {
    res.status(200).json(products);
+});
+
+router.get("/discounts", (req, res) => {
+   res.status(200).json(discounts);
 });
 
 router.get("/product-category", (req, res) => {
